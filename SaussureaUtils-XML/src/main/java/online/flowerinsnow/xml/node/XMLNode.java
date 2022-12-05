ea10190e6390238da5f6b1822a0763772a8c8d1c
冬花ice <flowerinsnow@donghua.email.cn>
2022-12-05 15:31:10 +0000

    1.3.1：修复javadoc格式错误导致生成错误的问题

--- SaussureaUtils-XML/src/main/java/online/flowerinsnow/xml/node/XMLNode.java
+++ SaussureaUtils-XML/src/main/java/online/flowerinsnow/xml/node/XMLNode.java
@@ -5,25 +5,19 @@ import org.w3c.dom.Node;
 /**
  * <p>代表一个XML节点</p>
  * <p>XML中所有的内容都是一个节点</p>
- * <p>
- *     <p>&lt;book&gt; - 这是一个文档节点</p>
- *     <p>&nbsp;&nbsp;&nbsp;&nbsp;&lt;!-- 123 --&gt; - 这是一个注释节点</p>
- *     <p>&nbsp;&nbsp;&nbsp;&nbsp;&lt;name&gt; - 这是一个元素节点</p>
- *     <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Java手册 - 这是一个文本节点</p>
- *     <p>&nbsp;&nbsp;&nbsp;&nbsp;&lt;/name&gt;</p>
- *     <p>&lt;/book&gt;</p>
- * </p>
+ * <p></p>
+ * <p>这一整个是一个文档节点</p>
+ * <p>&lt;book&gt; - 这是一个元素节点</p>
+ * <p>&nbsp;&nbsp;&nbsp;&nbsp;&lt;!-- 123 --&gt; - 这是一个注释节点</p>
+ * <p>&nbsp;&nbsp;&nbsp;&nbsp;&lt;name&gt; - 这是一个元素节点</p>
+ * <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Java手册 - 这是一个文本节点</p>
+ * <p>&nbsp;&nbsp;&nbsp;&nbsp;&lt;/name&gt;</p>
+ * <p>&lt;/book&gt;</p>
+ * <p></p>
  *
  * <p>其实&lt;id="10"/&gt;中的 'id="10"' 也是一个节点，但是这里直接将其当做属性来处理，这样方便一点</p>
  *
  * <p>每个节点都必须拥有根节点</p>
- * <p>
- *     <p>&lt;owner&gt;</p>
- *     <p>&nbsp;&nbsp;&nbsp;&nbsp;&lt;parent&gt;</p>
- *     <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;child>&lt;/child&gt;</p>
- *     <p>&nbsp;&nbsp;&nbsp;&nbsp;&lt;/parent&gt;</p>
- *     <p>&lt;/owner><p/>
- * </p>
  */
 public interface XMLNode {
     /**
