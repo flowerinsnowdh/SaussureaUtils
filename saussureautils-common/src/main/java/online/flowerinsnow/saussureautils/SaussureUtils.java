816be7ceaea72ed83aa03b3d2f3fa39fdd25bc8d
冬花ice <flowerinsnow@donghua.email.cn>
2023-03-16 15:10:39 +0800

    Update version

--- saussureautils-common/src/main/java/online/flowerinsnow/saussureautils/SaussureUtils.java
+++ saussureautils-common/src/main/java/online/flowerinsnow/saussureautils/SaussureUtils.java
@@ -3,5 +3,5 @@ package online.flowerinsnow.saussureautils;
 import org.jetbrains.annotations.NotNull;
 
 public class SaussureUtils {
-    @NotNull public static final String VERSION = "3.0.0";
+    @NotNull public static final String VERSION = "3.1.0";
 }
